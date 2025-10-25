d83a7d4b2a6352c94738eb16ca7410c33a7529b8
Mishell rose mathew <mishellrm06@gmail.com>
2025-10-25 13:14:40 +0530

    hotspot error

--- app/routers/hotspot.py
+++ app/routers/hotspot.py
@@ -8,6 +8,8 @@ from app.database import get_db
 router=APIRouter(prefix="/hotspot",tags=['Hotspot'])
 
 #create hotspot,add image,description and values in a single endpoint
+#the text data (name, bio) directly in the database
+#the image file in a folder (and save its path in the database)
 @router.post("/",status_code= status.HTTP_201_CREATED,response_model=schemas.HSOut)
 async def create_hotspot( hotspot: schemas.CreateHS ,file: UploadFile , db: Session = Depends(get_db)):
     db_image = db.query(models.Hotspot)
